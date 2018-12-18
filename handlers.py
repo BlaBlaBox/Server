3f28555a4ef5d72e1ee673c4e7fc16d1f7562c73
Alpkant <kantarcia@itu.edu.tr>
2018-12-18 13:12:59 +0300

    asdf

--- handlers.py
+++ handlers.py
@@ -201,7 +201,7 @@ def add_movie():
         absolute_path = os.path.abspath("./" + video_path)
         print("video_path=", video_path)
         print("absolute=", absolute_path)
-        video.save(video_path)
+        video.save(absolute_path)
 
     movie_json = {"movie_id":imdb_id,"rent":rent_price,"purchase":rent_price,"video_url":video_path}
     rv = requests.post(MOVIE + "movie/add",json=movie_json)
